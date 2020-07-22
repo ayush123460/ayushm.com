cf653aad2bd3a526c754d9198b62d950f1631ec6
Ayush Mukherjee <ayushm12360@gmail.com>
2020-07-23 01:23:00 +0530

    init scss; create header; create homepage

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <div class="brand">
+            <div class="logo">
+                <img src="@/assets/logo.png" alt="Ayush M. Logo" width="60px" height="60px">
+            </div>
+            <div class="name">
+                <span>Ayush</span>
+                <span>Mukherjee.</span>
+            </div>
+        </div>
+        <nav>
+            <router-link class="active" to="#work">work</router-link>
+            <router-link to="#about">about</router-link>
+            <router-link to="#skills">skills</router-link>
+            <router-link to="#contact">contact</router-link>
+        </nav>
+    </header>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="container">
+  <main>
+    <div class="tag">
+      I design and build
+      <br>
+      interfaces for Humans.
+    </div>
+    <div class="para">
+      <p>I make robust, easy to use systems and applications centered around humans. I believe simplicity is the way forward for modern systems and apps.</p>
+    </div>
+    <div class="cta">
+      <a href="#work" class="btn">See my work</a>
+      <a href="#contact" class="btn btn-secondary">Discuss a project</a>
+    </div>
+    <div class="illustration">
+      <img src="@/assets/illustration.png" alt="Illustration">
+    </div>
+  </main>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
